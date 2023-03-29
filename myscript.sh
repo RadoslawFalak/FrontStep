098755b1e34edfd175a567f90845de76106e0aaf
Radosław Falak <radoslaw.falak@gmail.com>
2023-03-30 01:30:09 +0200

    commitMessage

--- myscript.sh
+++ myscript.sh
@@ -1,6 +1,6 @@
 echo "What is the commit message ?"
 read commitMessage
-cd /Users/radoslawfalak/Desktop/GIT/FrontStep 
+cd Users/radoslawfalak/Desktop/GIT/FrontStep 
 git add .
 git commit -m commitMessage
 git push origin main
